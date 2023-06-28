e294181165b5bbc9dd04bf6cf5e3fcf621ac5e5a
algonouir <algo.mahdi.nouri@gmail.com>
2023-06-28 11:59:39 +0330

    create admin page create category add not original label to profuct page change core slice add original to label

--- pages/lists.tsx
+++ pages/lists.tsx
@@ -1,15 +1,64 @@
 import Header from "@/components/header";
 import Product from "@/components/product";
 import { useAppSelector } from "@/store/HOCs";
+import { categoryType } from "@/store/core/slice";
+import { Dispatch, SetStateAction, useState } from "react";
+
+function Category(props: {
+    title: string;
+    section: categoryType | number;
+    depth: number;
+    handler: Dispatch<SetStateAction<number>>;
+}) {
+    return (
+        <ul className="w-full rounded-xl flex items-start flex-col">
+            {typeof props.section === "number" ? (
+                <button
+                    onClick={() => props.handler(props.section)}
+                    style={{
+                        backgroundColor: props.depth === 0 ? "red" : "",
+                        color: props.depth === 0 ? "white" : "",
+                        borderBottom:
+                            props.depth === 0 ? "" : "1px solid #0002",
+                        borderTopWidth:
+                            props.depth === 0 ? "" : "1px solid #0002",
+                    }}
+                    className="mb-5 w-full px-5 py-2 flex items-start"
+                >
+                    <p>{props.title}</p>
+                </button>
+            ) : (
+                <div className="mx-5 bg-white w-full">
+                    {Object.entries(props.section).map(([title, section]) => (
+                        <Category
+                            title={title}
+                            section={section}
+                            depth={props.depth + 1}
+                        />
+                    ))}
+                </div>
+            )}
+        </ul>
+    );
+}
 
 export default function ListsPage() {
     const products = useAppSelector((store) => store.products);
+    const category = useAppSelector((store) => store.core.category);
+    const [filter, filterHandler] = useState(-1);
     return (
         <div className="flex flex-col space-y-5 h-screen">
             <Header />
             <div className="flex flex-row space-x-5 rtl:space-x-reverse grow p-5">
-                <div className="bg-bg-200 h-full w-96 rounded-xl p-5">
-                    <p>فیلتر انجام کار</p>
+                <div className="bg-bg-200 h-full w-96 rounded-xl p-5 overflow-scroll">
+                    {Object.entries(category).map(([name, data]) => (
+                        <Category
+                            title={name}
+                            section={data}
+                            depth={0}
+                            handler={filterHandler}
+                        />
+                    ))}
                 </div>
                 <div
                     style={{
--- pages/product/[product].tsx
+++ pages/product/[product].tsx
@@ -1,16 +1,15 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import Product from "@/components/product";
 import { useAppSelector } from "@/store/HOCs";
 import { useRouter } from "next/router";
 import { BiMessageSquareX } from "react-icons/bi";
 
 function ProductDetail(props: { name: string; amount: string }) {
     return (
-        <div className="h-44 bg-bg-200 rounded-xl flex items-center justify-center relative">
+        <div className="h-44 bg-accent-100/70 rounded-xl flex items-center justify-center relative">
             <p className="text-xl font-bold">{props.amount}</p>
-            <p className="absolute bottom-5 text-gray-500 text-lg">
-                {props.name}
-            </p>
+            <p className="absolute bottom-5 text-white text-lg">{props.name}</p>
         </div>
     );
 }
@@ -34,12 +33,16 @@ export default function ProductPage() {
     const user = useAppSelector((store) => store.account.user);
     const productId = router.query.product;
 
-    const product = useAppSelector((store) => store.products).find(
+    const tmp = useAppSelector((store) => store.products);
+    const product = tmp.find(
         (p) =>
             p.id === (typeof productId === "string" ? parseInt(productId) : -1)
     );
 
     if (product !== undefined) {
+        const products = tmp.filter(
+            (p) => p.category === product.category && p.id !== product.id
+        );
         return (
             <div className="flex flex-col items-center w-full">
                 <Header />
@@ -53,6 +56,13 @@ export default function ProductPage() {
                             <div className="flex flex-col space-y-5 w-full">
                                 <label className="text-xl font-bold text-right">
                                     {product.persianName}
+                                    {product.orginal ? (
+                                        <></>
+                                    ) : (
+                                        <small className="m-2 text-white bg-slate-300 p-1 rounded-xl absolute top-3 left-3">
+                                            غیر اصل
+                                        </small>
+                                    )}
                                 </label>
                                 <label className="text-gray-500 font-bold">
                                     {product.garanty}
@@ -121,7 +131,7 @@ export default function ProductPage() {
                             gridTemplateColumns:
                                 "repeat(auto-fill, minmax(200px, 1fr))",
                         }}
-                        className="grid gap-5"
+                        className="grid gap-5 mb-10"
                     >
                         {Object.entries(product.data).map(
                             ([key, value], index) => (
@@ -133,8 +143,18 @@ export default function ProductPage() {
                             )
                         )}
                     </div>
-                    <div className="bg-bg-200 w-full rounded-xl h-72 p-5">
-                        <label>محصولات مشابه</label>
+                    <div
+                        className="bg-prime-200 w-full rounded-xl h-96 relative
+                        justify-end items-end p-5 flex flex-row space-x-5 rtl:space-x-reverse"
+                    >
+                        <label className="absolute top-10 right-10 z-10 text-white text-xl">
+                            محصولات مشابه
+                        </label>
+                        {products.map((item) => (
+                            <div className="w-64 h-96 -mt-32">
+                                <Product {...item} />
+                            </div>
+                        ))}
                     </div>
                     <div className="flex flex-row space-x-5 rtl:space-x-reverse">
                         <div className="w-2/3 h-96 flex flex-col items-center justify-center space-y-5">
