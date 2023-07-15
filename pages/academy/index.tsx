559820e53658ee80fe6a3e310f8d1037d1d789bd
algonouir <algo.mahdi.nouri@gmail.com>
2023-07-15 13:34:46 +0330

    clear drafts of another pages

--- pages/academy/index.tsx
+++ pages/academy/index.tsx
@@ -9,32 +9,6 @@ export default function MainPage() {
     return (
         <div className="flex flex-col items-center pt-36">
             <Header />
-            <div className="container p-5 space-y-5">
-                <Banner images={[["full", ""]]} />
-                <ProductLists
-                    products={products}
-                    title={{ name: "انتخاب هوش مصنوعی" }}
-                />
-                <Banner
-                    images={[
-                        ["1/2", ["", "", ""]],
-                        ["1/2", ""],
-                    ]}
-                />
-                <Banner images={[["full", ["", "", ""]]]} />
-                <Banner
-                    images={[
-                        ["1/2", ["", "", ""]],
-                        ["1/2", ""],
-                    ]}
-                />
-                <Banner
-                    images={[
-                        ["1/2", ["", "", ""]],
-                        ["1/2", ""],
-                    ]}
-                />
-            </div>
             <Footer />
         </div>
     );
--- pages/caffeh/index.tsx
+++ pages/caffeh/index.tsx
@@ -1,40 +1,10 @@
 import Footer from "@/components/UI/footer";
 import Header from "@/components/UI/header";
-import Banner from "@/components/UI/banner";
-import ProductLists from "@/components/store/productsList";
-import { useAppSelector } from "@/store/HOCs";
 
 export default function MainPage() {
-    const products = useAppSelector((store) => store.products);
     return (
         <div className="flex flex-col items-center pt-36">
             <Header />
-            <div className="container p-5 space-y-5">
-                <Banner images={[["full", ""]]} />
-                <ProductLists
-                    products={products}
-                    title={{ name: "انتخاب هوش مصنوعی" }}
-                />
-                <Banner
-                    images={[
-                        ["1/2", ["", "", ""]],
-                        ["1/2", ""],
-                    ]}
-                />
-                <Banner images={[["full", ["", "", ""]]]} />
-                <Banner
-                    images={[
-                        ["1/2", ["", "", ""]],
-                        ["1/2", ""],
-                    ]}
-                />
-                <Banner
-                    images={[
-                        ["1/2", ["", "", ""]],
-                        ["1/2", ""],
-                    ]}
-                />
-            </div>
             <Footer />
         </div>
     );
