2c03e69d1cd4f21fad55aed468dd56d844675632
algonouir <algo.mahdi.nouri@gmail.com>
2023-07-24 18:31:19 +0330

    send products to server

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,5 +1,5 @@
 import { axiosNoUser } from "@/core/axios";
-import { useAppDispatch } from "@/store/HOCs";
+import { useAppDispatch, useAppSelector } from "@/store/HOCs";
 import { loginThunk } from "@/store/account/thunk";
 import { useState } from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
@@ -7,114 +7,109 @@ import { BsTelephone } from "react-icons/bs";
 import PhoneInput from "../UI/phoneInput";
 
 export default function LoginModal(props: { handler: any; message: string }) {
-    const [sms, smsHandler] = useState(false);
-    const [code, codeHandler] = useState("");
-    const [phone, phoneHandler] = useState<number | "">();
-
-    const dispatch = useAppDispatch();
-    return props.message === "" ? (
-        <></>
-    ) : (
-        <div
-            className="absolute h-screen top-0 w-screen z-50
+  const [sms, smsHandler] = useState(false);
+  const [code, codeHandler] = useState("");
+  const [phone, phoneHandler] = useState<number | "">();
+  const products = useAppSelector((store) => store.account.products);
+  const dispatch = useAppDispatch();
+  return props.message === "" ? (
+    <></>
+  ) : (
+    <div
+      className="absolute h-screen top-0 w-screen z-50
             backdrop-blur flex items-center justify-center bg-black/10"
-        >
-            <div
-                className="bg-bg-200 pb-5 rounded-xl relative
+    >
+      <div
+        className="bg-bg-200 pb-5 rounded-xl relative
                 space-y-5 flex flex-col items-center px-5"
-            >
-                <div className="flex flex-row items-center justify-center p-5">
-                    <button
-                        onClick={() => {
-                            props.handler("");
-                            smsHandler(false);
-                        }}
-                        className="absolute left-5"
-                    >
-                        <AiOutlineArrowLeft className="text-xl" />
-                    </button>
-                    <p>ورود به حساب کاربری</p>
-                </div>
-                <div className="flex flex-col space-y-5">
-                    <div
-                        style={{ direction: "ltr" }}
-                        className="space-x-2 flex flex-row w-fit"
-                    >
-                        <PhoneInput value={phone} handler={phoneHandler} />
-                        <button
-                            disabled={sms}
-                            onClick={
-                                sms
-                                    ? () => {}
-                                    : () => {
-                                          if (typeof phone === "number") {
-                                              axiosNoUser
-                                                  .post("login/", { phone })
-                                                  .then(() => smsHandler(true))
-                                                  .catch((error) =>
-                                                      console.log(error)
-                                                  );
-                                          }
-                                      }
-                            }
-                            className={`${
-                                sms ? "bg-prime-200 " : "bg-green-400"
-                            } p-3 rounded-xl w-44`}
-                        >
-                            <p>{sms ? "ارسال مجدد" : "دریافت کد تایید"}</p>
-                        </button>
-                    </div>
-                    {sms ? (
-                        <div
-                            style={{ direction: "ltr" }}
-                            className="space-x-2 flex flex-row w-fit"
-                        >
-                            <div
-                                className="flex bg-slate-200 items-center
-                        space-x-5 rounded-xl pl-3 rtl:space-x-reverse w-full"
-                            >
-                                <BsTelephone className="text-xl text-gray-700" />
-                                <input
-                                    type="text"
-                                    value={code}
-                                    onChange={(e) =>
-                                        codeHandler(e.target.value)
-                                    }
-                                    className="outline-none bg-slate-200 p-2 w-full"
-                                />
-                            </div>
-                            <button
-                                onClick={
-                                    sms
-                                        ? () => {
-                                              if (typeof phone === "number") {
-                                                  console.log("send sms");
-
-                                                  dispatch(
-                                                      loginThunk({
-                                                          phone,
-                                                          code,
-                                                      })
-                                                  );
-                                                  props.handler("");
-                                                  smsHandler(false);
-                                              }
-                                          }
-                                        : () => {}
-                                }
-                                className="p-3 rounded-xl w-44 bg-prime-300"
-                            >
-                                <p>تایید کد</p>
-                            </button>
-                        </div>
-                    ) : (
-                        <></>
-                    )}
-                </div>
-                <p className="text-center text-gray-500 text-sm">
-                    {props.message}
-                </p>
-            </div>
+      >
+        <div className="flex flex-row items-center justify-center p-5">
+          <button
+            onClick={() => {
+              props.handler("");
+              smsHandler(false);
+            }}
+            className="absolute left-5"
+          >
+            <AiOutlineArrowLeft className="text-xl" />
+          </button>
+          <p>ورود به حساب کاربری</p>
         </div>
-    );
+        <div className="flex flex-col space-y-5">
+          <div
+            style={{ direction: "ltr" }}
+            className="space-x-2 flex flex-row w-fit"
+          >
+            <PhoneInput value={phone} handler={phoneHandler} />
+            <button
+              disabled={sms}
+              onClick={
+                sms
+                  ? () => {}
+                  : () => {
+                      if (typeof phone === "number") {
+                        axiosNoUser
+                          .post("login/", { phone })
+                          .then(() => smsHandler(true))
+                          .catch((error) => console.log(error));
+                      }
+                    }
+              }
+              className={`${
+                sms ? "bg-prime-200 " : "bg-green-400"
+              } p-3 rounded-xl w-44`}
+            >
+              <p>{sms ? "ارسال مجدد" : "دریافت کد تایید"}</p>
+            </button>
+          </div>
+          {sms ? (
+            <div
+              style={{ direction: "ltr" }}
+              className="space-x-2 flex flex-row w-fit"
+            >
+              <div
+                className="flex bg-slate-200 items-center
+                        space-x-5 rounded-xl pl-3 rtl:space-x-reverse w-full"
+              >
+                <BsTelephone className="text-xl text-gray-700" />
+                <input
+                  type="text"
+                  value={code}
+                  onChange={(e) => codeHandler(e.target.value)}
+                  className="outline-none bg-slate-200 p-2 w-full"
+                />
+              </div>
+              <button
+                onClick={
+                  sms
+                    ? () => {
+                        if (typeof phone === "number") {
+                          console.log("send sms");
+
+                          dispatch(
+                            loginThunk({
+                              phone,
+                              code,
+                              products,
+                            })
+                          );
+                          props.handler("");
+                          smsHandler(false);
+                        }
+                      }
+                    : () => {}
+                }
+                className="p-3 rounded-xl w-44 bg-prime-300"
+              >
+                <p>تایید کد</p>
+              </button>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+        <p className="text-center text-gray-500 text-sm">{props.message}</p>
+      </div>
+    </div>
+  );
 }
--- store/account/thunk.ts
+++ store/account/thunk.ts
@@ -3,53 +3,53 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { OrderProduct } from "./slice";
 
 export const addToCartThunk = createAsyncThunk(
-    "addToCartThunk",
-    async (
-        actionData: { product: number; count: number; select: number },
-        { rejectWithValue }
-    ) => {
-        const response = await axiosUser.post("cart/", actionData);
-        if (response.status === 202) {
-            return {
-                ...actionData,
-                ...response.data,
-            };
-        } else {
-            return rejectWithValue(response.status);
-        }
+  "addToCartThunk",
+  async (
+    actionData: { product: number; count: number; select: number },
+    { rejectWithValue }
+  ) => {
+    const response = await axiosUser.post("cart/", actionData);
+    if (response.status === 202) {
+      return {
+        ...actionData,
+        ...response.data,
+      };
+    } else {
+      return rejectWithValue(response.status);
     }
+  }
 );
 
 export const loginThunk = createAsyncThunk(
-    "loginThunk",
-    async (
-        actionData: { code: string; phone: number },
-        { rejectWithValue }
-    ) => {
-        const response = await axiosNoUser.post("login/", actionData);
-        const { user, access, orders } = response.data;
-        axiosUser.defaults.headers.Authorization = `Bearer ${access}`;
-        return { user, orders };
-    }
+  "loginThunk",
+  async (
+    actionData: { code: string; phone: number; products: OrderProduct[] },
+    { rejectWithValue }
+  ) => {
+    const response = await axiosNoUser.post("login/", actionData);
+    const { user, access, orders } = response.data;
+    axiosUser.defaults.headers.Authorization = `Bearer ${access}`;
+    return { user, orders };
+  }
 );
 
 export const changeAccountDataThunk = createAsyncThunk(
-    "changeAccountDataThunk",
-    async (
-        actionData: {
-            fName: string;
-            lName: string;
-            nationalCode: string;
-            email: string;
-            address: string;
-        },
-        { rejectWithValue }
-    ) => {
-        const response = await axiosUser.put("user/", actionData);
-        if (response.status === 204) {
-            return actionData;
-        } else {
-            return rejectWithValue(response.status);
-        }
+  "changeAccountDataThunk",
+  async (
+    actionData: {
+      fName: string;
+      lName: string;
+      nationalCode: string;
+      email: string;
+      address: string;
+    },
+    { rejectWithValue }
+  ) => {
+    const response = await axiosUser.put("user/", actionData);
+    if (response.status === 204) {
+      return actionData;
+    } else {
+      return rejectWithValue(response.status);
     }
+  }
 );
