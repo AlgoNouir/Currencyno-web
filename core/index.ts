084e7c07e2413105e59c83990dcb3131e7bd68b2
algonouir <algo.mahdi.nouri@gmail.com>
2023-09-25 15:01:09 +0330

    change post price

--- core/index.ts
+++ core/index.ts
@@ -1,3 +1,3 @@
 export const SERVER_URL = "https://currencyno-server.iran.liara.run/";
 // export const SERVER_URL = "http://127.0.0.1:8000/";
-export const postPrice = 40000;
+export const postPrice = 45000;
