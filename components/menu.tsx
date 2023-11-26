e1de78690fd0a373d141f93f93fc941f01b59775
AlgoNouir-thinkpad-ubuntu <algo.mahdi.nouri@gmail.com>
2023-11-26 22:12:20 +0330

    ...

--- components/menu.tsx
+++ components/menu.tsx
@@ -4,19 +4,16 @@ import { categoryType } from "@/store/account/slice";
 type MenuItem = Required<MenuProps>["items"][number];
 
 export function menuDirector(array: categoryType[]): MenuItem[] {
+  const result: MenuItem[] = [];
 
-  
-  const result: MenuItem[] = array.filter(c => c.parent === null).map(c => {
-    let tmp =array.filter(cc => cc.parent == c.id).map(cc => ({
-      label:cc.name,
-      key:cc.id
-    }))
-
-    return({
-    label: c.name,
-    key: c.id,
-    children: tmp.length ===0?undefined:tmp
-  })})
-
+  array.forEach((c) => {
+    const children = array.filter((child) => child.parent === c.id);
+    if (!result.map((r) => r?.key).includes(c.parent))
+      result.push({
+        label: c.name,
+        key: c.id,
+        children: children.length === 0 ? undefined : menuDirector(children),
+      });
+  });
   return result;
 }
